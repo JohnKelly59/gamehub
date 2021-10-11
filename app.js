4d0a0d320da12e87ad1033eb3789940ae0c78835
John K <john.kellyfitness@gmail.com>
2021-10-11 09:55:23 -0400

    App connects to the api key found in another file

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const  path = require('path');
 const app = express();
 require("dotenv").config()
 var https = require('https');
-
+var config = require('./config');
 
  app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({
