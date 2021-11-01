9083a7469dfd6d56b7d224917be6c0743ad30ed8
John K <john.kellyfitness@gmail.com>
2021-10-31 20:09:32 -0400

    trying to deploy

--- app.js
+++ app.js
@@ -131,8 +131,5 @@ res.redirect("/drum");
 
 
 
-      const server = app.listen(process.env.PORT || 8081, () => {
-        const port = server.address().port;
-        console.log(`Express is working on port ${port}`);
-      });
-
+     app.listen(process.env.PORT || 5000);
+    module.exports = app
