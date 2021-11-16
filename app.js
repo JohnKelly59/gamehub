0c502eddbd9578001e34d8cd55e65f48a155d706
John K <john.kellyfitness@gmail.com>
2021-11-16 10:46:38 -0500

    adding

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const  path = require('path');
 const app = express();
+require("dotenv").config()
+var https = require('https');
+var config = require('./config');
 
  app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({
@@ -9,6 +12,8 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.set('view engine', 'ejs');
+
+
 
 
 app.post("/home", function(req, res){
@@ -65,6 +70,66 @@ res.redirect("/drum");
 
     res.redirect("/ttt");
       });
+
+
+
+      app.post("/mailC", function(req, res){
+
+      res.redirect("/mail");
+        });
+
+      app.get("/mail", function(req, res) {
+        res.render("signup");
+      });
+
+      app.post("/send", function(req, res) {
+        const first = req.body.firstName;
+        const last = req.body.lastName;
+        const email = req.body.email;
+        console.log(first, last, email);
+
+        const data = {
+          members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+              FNAME: first,
+              LNAME: last,
+            }
+
+          }
+        ]
+      };
+      const jsonData = JSON.stringify(data);
+      const url = "https://us5.api.mailchimp.com/3.0/lists/92d5775add";
+
+      const options = {
+        method: "POST",
+        auth: config.MY_KEY
+      }
+      const request = https.request(url, options, function(response){
+      if (response.statusCode === 200) {
+        res.render("success")
+      }
+      else {
+        res.render("failure")
+      }
+
+        response.on("data", function(data){
+          console.log(JSON.parse(data));
+        })
+      })
+
+      request.write(jsonData);
+      request.end();
+
+      });
+
+      app.post("/retry", function(req, res){
+        res.redirect("/mail");
+      })
+
+
 
 
 app.listen(process.env.PORT || 5000);
